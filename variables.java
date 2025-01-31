67dd7ddc60f2a0c651a92011b4fe8215d538fb68
ayushreevyas <ayushreevyas1@gmail.com>
2025-01-31 17:02:37 +0530

    learn about variables

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int sum = a + b;
+        System.out.println(sum);
+        int diff = a - b;
+        System.out.println(diff);
+    }
+}
